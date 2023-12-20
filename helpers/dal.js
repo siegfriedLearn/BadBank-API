cb6107f932ebce2c566c7214f0c892c135405575
Carlos Medina <carlosmedina@192.168.0.3>
2023-12-19 19:39:43 -0500

    Cambiando push transacciones

--- helpers/dal.js
+++ helpers/dal.js
@@ -1,56 +1,66 @@
 const { parse } = require("dotenv");
 const { app } = require("../config/firebase");
-const { getDatabase, ref, set, get, push, update, child } = require("firebase/database");
-    
-    
-const searchByUid =  (uid) =>{
-    const dbRef = ref(getDatabase());
+const {
+  getDatabase,
+  ref,
+  set,
+  get,
+  push,
+  update,
+  child,
+} = require("firebase/database");
 
-    return new Promise((resolve, reject) => {
-        get(child(dbRef, `users/${uid}`)).then((snapshot) => {
-            if (snapshot.exists()) {
-                const history = snapshot.val();
-                //console.log(history);
-      
-              resolve(history)
-      
-            } else {
-              const error = "No data available";
-              reject(error)
-            }
-          }).catch((error) => {
-            console.error(error);
-            reject(error);
-          });
-    })
-    
+const searchByUid = (uid) => {
+  const dbRef = ref(getDatabase());
+
+  return new Promise((resolve, reject) => {
+    get(child(dbRef, `users/${uid}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const history = snapshot.val();
+          //console.log(history);
+
+          resolve(history);
+        } else {
+          const error = "No data available";
+          reject(error);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      });
+  });
 };
 
-const changeBalance = (uid, balance) =>{
-    const db = getDatabase();
+const changeBalance = (uid, balance) => {
+  const db = getDatabase();
 
-    return new Promise((resolve, reject) => {
-        update(ref(db, "/users/" + uid), {balance})
-            .then(resp => resolve('Ok'))
-            .catch(err=>reject('error'));
-    })
+  return new Promise((resolve, reject) => {
+    update(ref(db, "/users/" + uid), { balance })
+      .then((resp) => resolve("Ok"))
+      .catch((err) => reject("error"));
+  });
 };
 
-const addTransaction = (uid, tipo, value) =>{
-    const db = getDatabase();
+const addTransaction = (uid, tipo, value) => {
+  const db = getDatabase();
 
-    return new Promise((resolve, reject) => {
-        const date = new Date();
-        push(ref(db, "/users/" + uid + "/transacciones"), { tipo, value, fecha: String(date) })
-            .then(resp => resolve('Ok.'))
-            .catch(err=>reject('error'));
-    })
+  return new Promise((resolve, reject) => {
+    const date = new Date();
+    // push(child(db, "/users/" + uid + "/transacciones"),  {tipo, value, fecha: String(date)} )
+    //     .then(resp => resolve('Ok.'))
+    //     .catch(err=>reject('error'))
+    const referencia = ref(db, "/users/" + uid);
+    //const transacciones = referencia(child("transacciones"));
+    push(referencia, {tipo, value, fecha: String(date)})
+    //transacciones.push({tipo, value, fecha: String(date)}, {merge: true})
+
+  });
 };
-
 
 module.exports = {
-    searchByUid,
-    changeBalance,
-    addTransaction
-  };
-     
+  searchByUid,
+  changeBalance,
+  addTransaction,
+};
